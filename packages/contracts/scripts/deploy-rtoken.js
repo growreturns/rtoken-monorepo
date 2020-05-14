7c51aac17392f4140d4b108e1bb9527e2a9bd1e5
akshay-srivastav <akshay@somish.com>
2020-05-14 16:06:46 +0530

    custom rtoken deployment, rdai deployed

---
+++ packages/contracts/scripts/deploy-rtoken.js
@@ -0,0 +1,54 @@
+/**
+ * @dev Interactive script to deploy a fullset of rtoken contracts
+ */
+module.exports = async function (callback) {
+    try {
+        const { promisify } = require("util");
+        const rl = require("./common/rl");
+        const { web3tx } = require("@decentral.ee/web3-test-helpers");
+
+        global.web3 = web3;
+
+        const CompoundAllocationStrategy = artifacts.require("CompoundAllocationStrategy");
+        const RToken = artifacts.require("RToken");
+        const Proxy = artifacts.require("Proxy");
+
+        let newRtokenName = await promisify(rl.question)("Provide RToken Name/Description: ");
+        let newRtokenSymbol = await promisify(rl.question)("Provide RToken Symbol: ");
+        let cTokenAddress = await promisify(rl.question)("Provide cToken Address: ");
+
+        if (!newRtokenName || !newRtokenSymbol || !cTokenAddress) {
+            console.log("Invalid Input!!! Returning");
+            callback();
+        }
+
+        // Deploy compound's allocation strategy
+        let compoundAS = await web3tx(
+            CompoundAllocationStrategy.new,
+            `CompoundAllocationStrategy.new cToken ${cTokenAddress}`)(
+            cTokenAddress
+        );
+        console.log("compoundAllocationStrategy deployed at: ", compoundAS.address);
+
+        // Deploy RToken logic/implementation
+        let rToken = await web3tx(RToken.new, "rToken.new")();
+        console.log(`rToken (${newRtokenSymbol}) deployed at: `, rToken.address);
+
+        // Deploy RToken itself as proxy
+        const rTokenConstructCode = rToken.contract.methods.initialize(compoundAS.address, newRtokenName, newRtokenSymbol, 18)
+            .encodeABI();
+        console.log(`rTokenConstructCode rToken.initialize(${rTokenConstructCode})`);
+        const proxy = await web3tx(Proxy.new, "Proxy.new")(
+            rTokenConstructCode, rToken.address
+        );
+        console.log(`${newRtokenSymbol} proxy deployed at: ${proxy.address} at Block Number: ${proxy.receipt.blockNumber}`);
+
+        // transfer ownership of compoundAS from admin to new rToken proxy
+        console.log(`transfer ownership of compoundAS to new rToken(${newRtokenSymbol} proxy)`, proxy.address);
+        await web3tx(compoundAS.transferOwnership, "compoundAS.transferOwnership")(proxy.address);
+
+        callback();
+    } catch (err) {
+        callback(err);
+    }
+};
